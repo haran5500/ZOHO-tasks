f630cb443bc5dbb92013d231a52a0aa41454e22f
haran5500 <65752213+haran5500@users.noreply.github.com>
2022-01-07 19:06:02 +0530

    Add files via upload

---
+++ jdbcprojects/src/userexception/CustomException.java
@@ -0,0 +1,13 @@
+package userexception;
+
+public class CustomException extends Exception
+{
+	public CustomException(String message)
+	{
+		super(message);
+	}
+	public CustomException(Exception ex)
+	{
+		super(ex);
+	}
+}
